aef3b9221f7c641ceedb6cd2579e366c2e9559e2
Eclixal <28450187+eclixal@users.noreply.github.com>
2022-08-14 22:07:35 +0200

    add routes

--- strawberrycorps/routes.go
+++ strawberrycorps/routes.go
@@ -10,6 +10,7 @@ func InitRoutes(router *gin.Engine) {
 	router.POST("/api/game", rest.CreateGame)
 	router.DELETE("/api/game/:id", rest.DeleteGame)
 	router.GET("/api/game/:id/logs", rest.GetGameLogs)
+	router.GET("/api/game/:id", nil)
 
 	router.GET("/api/health", rest.Heatlh)
 	router.GET("/api/health/:id", rest.GetGamesHealth)
